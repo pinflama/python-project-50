866af56d17678014407d54796823b54f1aa018c4
Flamingo Pink <pinflama@gmail.com>
2023-11-09 15:26:28 +0000

    fix_step2

--- gendiff/tools/argparse.py
+++ gendiff/tools/argparse.py
@@ -9,7 +9,7 @@ def parser_arg():
         files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', default='stylish',
-                        help='output format (default: "stylish")')
+    parser.add_argument('-f', '--format',
+                        help='set format of output')
     args = parser.parse_args()
     return args.first_file, args.second_file, args.format
